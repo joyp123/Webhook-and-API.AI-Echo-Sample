497d00c9551273b1d037288d24aa39c14e76e5e4
Joy Prakash <joy.prakash@nxp.com>
2018-09-21 23:56:37 +0530

    changes

--- index.js
+++ index.js
@@ -14,17 +14,8 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  var speech =
-    req.body.result &&
-    req.body.result.parameters &&
-    req.body.result.parameters.echoText
-      ? req.body.result.parameters.echoText
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    speech: speech,
-    displayText: speech,
-    source: "webhook-echo-sample"
-  });
+  
+  return res.json({fulfillmentText: "success"});
 });
 
 restService.post("/audio", function(req, res) {
